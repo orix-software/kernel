34c2f5458badda377d02b7e2bf723079a109a747
jede <jede@oric.org>
2020-04-16 13:15:17 +0200

    Fix bug when we launch binary from sdcard

--- src/functions/xexec.asm
+++ src/functions/xexec.asm
@@ -1,12 +1,5 @@
 .proc _XEXEC
 
-; DOC_PRIMITIVE_NAME=XEXEC
-; DOC_REGISTERS_MODIFIED=A,X,Y
-; DOC_PARAMETERS_REGISTERS_IN_0=A,Y
-; DOC_PARAMETERS_REGISTERS_COMMENTS_0=A and Y contains the pointer of the string to execute
-; DOC_MEMORY_WRITE_0=
-; DOC_MEMORY_WRITE_1=
-
 ;PARSE_VECTOR
     ; A & Y contains the string command to execute
     sta     TR0        ; Save string pointer
@@ -44,8 +37,6 @@ next:
     
     bne     next_bank
 
-
-
     lda     TR0
     ldy     TR1
     jsr     kernel_try_to_find_command_in_bin_path
@@ -56,21 +47,23 @@ next:
 
 ; Exit
 
-    ldx     KERNEL_KERNEL_XEXEC_BNKOLD
-    stx     BNKOLD
+;    ldx     KERNEL_KERNEL_XEXEC_BNKOLD
+ ;   stx     BNKOLD
 
     rts
 
 out1:
 
-    ; Now kill the current process
+;    ; Now kill the current process
     lda     kernel_process+kernel_process_struct::kernel_current_process
     jsr     kernel_kill_process
+;
     ; Back to calling bank
 exit:  
     lda     KERNEL_KERNEL_XEXEC_BNKOLD
     sta     BNKOLD
-    ;sta     BNK_TO_SWITCH    
+    sta     BNK_TO_SWITCH    
+  
     lda     #EOK
     rts
 .endproc
