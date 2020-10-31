139d5dbeed49425b84c7f85c3e505b2066d45399
jede <jede@oric.org>
2020-10-31 23:35:21 +0100

    add KERNEL_ERRNO to XVARS_ROUTINE

--- src/functions/xvars.asm
+++ src/functions/xvars.asm
@@ -10,10 +10,13 @@ XVARS_TABLE_LOW:
   .byt     <kernel_malloc
   .byt     <KERNEL_CH376_MOUNT
   .byt     <KERNEL_CONF_BEGIN
+  .byt     <KERNEL_ERRNO
   
 XVARS_TABLE_HIGH:
   .byt     >kernel_process
   .byt     >kernel_malloc
   .byt     >KERNEL_CH376_MOUNT
   .byt     >KERNEL_CONF_BEGIN
+  .byt     >KERNEL_ERRNO
+  
   
