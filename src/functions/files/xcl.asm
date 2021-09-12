7ca9816c565d6a30bdd1e76445645b83bd464cf1
jede <jede@oric.org>
2021-09-12 19:05:41 +0200

    Clean XCL routine

--- src/functions/files/xcl.asm
+++ src/functions/files/xcl.asm
@@ -1,41 +1,31 @@
 XCL0_ROUTINE
-	ldx     #$00
-	.byt    $2c
-XCL1_ROUTINE	
-	ldx     #$04
-	.byt    $2c
-XCL2_ROUTINE	
-	ldx     #$08
-	.byt    $2c	
-XCL3_ROUTINE
-
-	ldx     #$0c
-	ldy     #$03
-	cmp     #$00
-	beq     Lc74e
-@loop:
-	cmp     IOTAB0,x
-	beq     Lc73b 
-	inx
-	dey
-	bpl     @loop
-Lc73a	
+	;ldx     #$00
+	;ldy     #$03
+	;cmp     #$00
+	;beq     Lc74e
+;@loop:
+;	cmp     IOTAB
+;	beq     Lc73b 
+	;inx
+	;dey
+	;bpl     @loop
+;Lc73a:	
 	rts
-Lc73b	
-	lsr     IOTAB0,x
-	ldx     #$0F
-@L1:	
-	cmp     IOTAB0,x
-	beq     Lc73a
-	dex
-	bpl     @L1
-	tax
-	ldy     #$81
-	jmp     send_command_A
-Lc74e	
-	lsr     IOTAB0,x
-	inx
-	dey
-	bpl     Lc74e
-	rts
+;Lc73b:	
+;	lsr     IOTAB
+;	ldx     #$0F
+;@L1:	
+;	cmp     IOTAB
+;	beq     Lc73a
+;	dex
+;	bpl     @L1
+	;tax
+	;ldy     #$81
+	;jmp     send_command_A
+;Lc74e:	
+;	lsr     IOTAB0,x
+	;inx
+	;dey
+	;bpl     Lc74e
+	;rts
 	
