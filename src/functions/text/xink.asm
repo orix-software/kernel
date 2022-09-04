f0e8fee85465dd1e84c8dad97b0779b216e5cd95
jede <jede@oric.org>
2022-09-04 23:05:56 +0200

    cleaning

--- src/functions/text/xink.asm
+++ src/functions/text/xink.asm
@@ -7,7 +7,7 @@
 ;Principe:A contient la couleur, X la fenêtre ou 128 si mode HIRES et C=1 si la
 ;couleur est pour l'encre, 0 pour le fond.
 ;         Changer la couleur consiste à remplir la colonne couleur correspondante
-;         avec le code de couleur. Auncun test de validit? n'étant fait, on peut
+;         avec le code de couleur. Auncun test de validite n'étant fait, on peut
 ;         utiliser ce moyen pour remplir les colonnes 0 et 1 de n'importe quel
 ;         attribut.
 
@@ -18,59 +18,59 @@
   bmi     @S4       ; oui ----------------------------------------------
   stx     SCRNB     ; TEXT, on met le numero de fenetre dans $28       I
   bcc     @S2       ; si C=0, c'est PAPER                              I
-  sta     SCRCT,X   ; on stocke la couleur d'encre                     I
+  sta     SCRCT,x   ; on stocke la couleur d'encre                     I
   bcs     @S1       ; si C=1 c'est INK                                 I
 @S2:
-  sta     SCRCF,X   ; ou la couleur de fond  
+  sta     SCRCF,x   ; ou la couleur de fond
 @S1:
-  lda     FLGSCR,X  ; est on en 38 colonnes ?                          I
+  lda     FLGSCR,x  ; est on en 38 colonnes ?                          I
   and     #$10      ;                                                   I
   bne     @S3       ; mode 38 colonnes ------------------------------  I
   lda     #$0C      ;  mode 40 colonnes, on efface l'ecran           I  I
-  jsr     Ldbb5     ;  (on envoie CHR$(12))                          I  I 
+  jsr     Ldbb5     ;  (on envoie CHR$(12))                          I  I
   lda     #$1D      ;  et on passe en 38 colonnes                    I  I
-  jsr     Ldbb5     ;  (on envoie CHR$(29))                          I  I 
+  jsr     Ldbb5     ;  (on envoie CHR$(29))                          I  I
   ldx     SCRNB     ;  on prend X=numéro de fen?tre                  I  I
 @S3:
-  lda     SCRDY,X           ;  on prend la ligne 0 de la fenêtre <------------  I
-  jsr     XMUL40_ROUTINE    ;  *40 dans RES                                     I 
-  lda     SCRBAL,X          ;  AY=adresse de base de la fenêtre                 I
-  ldy     SCRBAH,X          ;                                                   I
-  jsr     XADRES_ROUTINE    ;  on ajoute l'adresse à RES (ligne 0 *40) dans RES I 
-  ldy     SCRDX,X           ;  on prend la première colonne de la fenêtre       I
+  lda     SCRDY,x           ;  on prend la ligne 0 de la fenêtre <------------  I
+  jsr     XMUL40_ROUTINE    ;  *40 dans RES                                     I
+  lda     SCRBAL,x          ;  AY=adresse de base de la fenêtre                 I
+  ldy     SCRBAH,x          ;                                                   I
+  jsr     XADRES_ROUTINE    ;  on ajoute l'adresse à RES (ligne 0 *40) dans RES I
+  ldy     SCRDX,x           ;  on prend la première colonne de la fenêtre       I
   dey                       ;  on enlève deux colonnes                          I
   dey                       ;                                                   I
   sec                       ;                                                   I
-  lda     SCRFY,X           ;   on calcule le nombre de lignes                   I
-  sbc     SCRDY,X           ;   de la fenêtre                                    I
+  lda     SCRFY,x           ;   on calcule le nombre de lignes                   I
+  sbc     SCRDY,x           ;   de la fenêtre                                    I
   tax                       ;   dans X                                           I
   inx                       ;                                                    I
   tya                       ;   colonne 0 dans Y                                 I
-  bcs     @S5               ;   inconditionnel --------------------------------- I 
+  bcs     @S5               ;   inconditionnel --------------------------------- I
 @S4:
   lda     #$00              ;  <----------------------------------------------+-- FIXME 65C02
-  ldx     #$A0              ;                                                 I  
-  sta     RES               ;  RES=$A000 , adresse HIRES                      I  
-  stx     RES+1             ;                                                  I  
-  ldx     #$C8              ;   X=200 pour 200 lignes                          I  
-  lda     #$00              ;   A=0 pour colonne de début = colonne 0          I  
+  ldx     #$A0              ;                                                 I
+  sta     RES               ;  RES=$A000 , adresse HIRES                      I
+  stx     RES+1             ;                                                  I
+  ldx     #$C8              ;   X=200 pour 200 lignes                          I
+  lda     #$00              ;   A=0 pour colonne de début = colonne 0          I
 @S5:
-  plp         ;   on sort C <-------------------------------------  
-  adc     #$00    ;   A=A+C                                             
-  tay        ;    dans Y                                            
-  pla        ;    on sort le code                                   *
+  plp                       ;   on sort C <-------------------------------------
+  adc     #$00              ;   A=A+C
+  tay                       ;    dans Y
+  pla                       ;    on sort le code                                   *
 @S7:
-  sta     (RES),Y; -->on le place dans la colonne correspondante        
-  pha        ; I  on le sauve                                       
-  clc        ; I                                                    
-  lda     RES    ; I  on passe 28 colonnes                              
-  adc     #$28    ;I  (donc une ligne)                                  
-  sta     RES     ;I                                                    
-  bcc     @S6  ; I                                                    
-  inc     RES+1    ; I                                                    
+  sta     (RES),Y; -->on le place dans la colonne correspondante
+  pha        ; I  on le sauve
+  clc        ; I
+  lda     RES    ; I  on passe 28 colonnes
+  adc     #$28    ;I  (donc une ligne)
+  sta     RES     ;I
+  bcc     @S6  ; I
+  inc     RES+1    ; I
 @S6:
-  pla        ; I  on sort le code                                   
-  dex        ; I  on compte X lignes                                
+  pla        ; I  on sort le code
+  dex        ; I  on compte X lignes
   bne     @S7   ;---
   rts         ;   et on sort----------------------------------------
 .endproc
--- src/functions/text/xscrob_scroh.asm
+++ src/functions/text/xscrob_scroh.asm
@@ -1,63 +1,63 @@
 
-XSCROH_ROUTINE
-;                     SCROLLE UNE FENETRE VERS LE BAS                       
+XSCROH_ROUTINE:
+;                     SCROLLE UNE FENETRE VERS LE BAS
 ;Action:scrolle vers le bas de la ligne X à la ligne Y la fenêtre courante.
 
-  lda     #$00     ;  on prend $0028, soit 40                           
-  sta     DECFIN+1                                                          
-  lda     #$28                                                         
-  bne     LDE62    ;  inconditionnel                                    
+  lda     #$00     ;  on prend $0028, soit 40
+  sta     DECFIN+1
+  lda     #$28
+  bne     LDE62    ;  inconditionnel
 
 
-;                      SCROLLE UNE FENETRE VERS LE HAUT                      
-;Action:scrolle vers le haut de la ligne X à la ligne Y la fenetre courante.     
+;                      SCROLLE UNE FENETRE VERS LE HAUT
+;Action:scrolle vers le haut de la ligne X à la ligne Y la fenetre courante.
 
 XSCROB_ROUTINE:
-  lda     #$FF       ;    on prend $FFD8, soit -40 en complement a 2        fixme 
-  sta     DECFIN+1                                                          
-  lda     #$D8                                                         
+  lda     #$FF       ;    on prend $FFD8, soit -40 en complement a 2
+  sta     DECFIN+1
+  lda     #$D8
 LDE62:
-  sta     DECFIN     ;   $06-07 contiennent le déplacement                 
-  stx     RES        ;    on met la ligne de depart en RES                  
-  tya                                                              
-  sec                                                              
-  sbc     RES        ;   on calcule le nombre de lignes                    
-  pha               ;    on sauve le nombre de lignes                      
-  txa            ;    ligne de début dans A                             
-  bit     DECFIN                                                          
-  bpl     @skip      ;    déplacement negatif ?                             
-  tya            ;    oui, ligne de fin dans A
+  sta     DECFIN     ;   $06-07 contiennent le déplacement
+  stx     RES        ;    on met la ligne de depart en RES
+  tya
+  sec
+  sbc     RES        ;   on calcule le nombre de lignes
+  pha                ;    on sauve le nombre de lignes
+  txa                ;    ligne de début dans A
+  bit     DECFIN
+  bpl     @skip      ;    déplacement negatif ?
+  tya                ;    oui, ligne de fin dans A
 @skip:
-  ldx     SCRNB                                                          
-  jsr     LDE12      ;   on calcule l'adresse de la ligne                 
-  clc                                                              
-  adc     SCRDX,X    ;   l'adresse exacte de la ligne dans la fen?tre      
-  bcc     @skip2                                                       
+  ldx     SCRNB
+  jsr     LDE12      ;   on calcule l'adresse de la ligne
+  clc
+  adc     SCRDX,x    ;   l'adresse exacte de la ligne dans la fenetre
+  bcc     @skip2
   iny
 @skip2:
-  sta     DECCIB     ;  est dans $08-09                                   
-  sty     DECCIB+1                                                          
-  clc            ; on ajoute le d?placement                          
-  adc     DECFIN                                                          
+  sta     DECCIB     ;  est dans $08-09
+  sty     DECCIB+1
+  clc            ; on ajoute le deplacement
+  adc     DECFIN
   sta     DECDEB
-  tya                                                              
-  adc     DECFIN+1                                                          
-  sta     DECDEB+1   ;   dans $04-05                                       
-  pla                ;   on sort le nombre de lignes                       
-  sta     RES        ;   dans RES                                          
+  tya
+  adc     DECFIN+1
+  sta     DECDEB+1   ;   dans $04-05
+  pla                ;   on sort le nombre de lignes
+  sta     RES        ;   dans RES
   beq     LDEC4      ;   si nul on fait n'importe quoi ! on devrait sortir!
   bmi     LDECD      ;  si négatif, on sort ------------------------------
   sec                ;  on calcule                                       I
   ldx     SCRNB      ;                                                   I
-  lda     SCRFX,X    ; la largeur de la fenètre                          I
-  sbc     SCRDX,X    ;                                                   I
+  lda     SCRFX,x    ; la largeur de la fenètre                          I
+  sbc     SCRDX,x    ;                                                   I
   sta     RES+1      ;  dans RES+1                                       I
-LDE9D
-  ldy     RES+1 
-LDE9F            ;                                                   I
-  lda     (DECDEB),Y ;   on transfère une ligne                          I
-  sta     (DECCIB),Y ;                                                   I
-  DEY                ;                                                   I
+LDE9D:
+  ldy     RES+1
+LDE9F:            ;                                                   I
+  lda     (DECDEB),y ;   on transfère une ligne                          I
+  sta     (DECCIB),y ;                                                   I
+  dey                ;                                                   I
   bpl     LDE9F      ;                                                   I
   clc                ;                                                   I
   lda     DECDEB     ;   on ajoute le déplacement                        I
@@ -80,7 +80,7 @@ LDEC4
   lda     #$20       ;                                                   I
 @L1:
   sta     (DECCIB),Y ;  avec de espaces                                  I
-  DEY                ;                                                   I
+  dey                ;                                                   I
   bpl     @L1        ;                                                   I
 LDECD
   rts                ;  <-------------------------------------------------
